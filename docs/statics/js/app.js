95c703ea8abb48fd1a4a3d35da278544a3959c95
erickuu <erickgraterol377@gmail.com>
2021-11-03 13:04:59 +0100

    Meger docs

---
+++ docs/statics/js/app.js
@@ -0,0 +1,16 @@
+const apiKey = '7b3405e1';
+let movie = prompt('Busque una Pelicula : ');
+const urlApi = `http://www.omdbapi.com/?apikey=${apiKey}&s=${movie}`;
+	
+fetch(urlApi)
+	.then(resp => resp.json())
+	.then(data => watch(data))
+	.catch(err => new Error(err));
+
+let watch = (data)=>{
+	let body = '';
+	for(var i = 0 ; i < data.Search.length; i++){
+		body += `<tr><td>${data.Search[i].Title}</td><td>${data.Search[i].Year}</td><td><img src="${data.Search[i].Poster}" alt=""></td></tr>`;
+		document.getElementById('tbody').innerHTML = body;
+	}
+}
